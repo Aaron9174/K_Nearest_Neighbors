4c9d4bd53e69330b917a578c4f114e83fb2add4b
Aaron Hebson <Aarommh7@gmail.com>
2020-04-09 14:53:19 -0400

    Examples of k nearest neighbors that uses sci-learn to show and give an example on how this works.

--- Example/index.py
+++ Example/index.py
@@ -17,11 +17,13 @@
 #     and Software 1, 1992, 23-34 (Gordon & Breach Science Publishers).
 # - Sentdex from YT
 
+import os
 import numpy as np
 from sklearn import preprocessing, model_selection, neighbors
 import pandas as pd
 
-df = pd.read_csv("breast-cancer-wisconsin.data")
+#my_path = os.path.abspath(os.path.dirname("breast-cancer-wisconsin.data"))
+df = pd.read_csv("../breast-cancer-wisconsin.data")
 df.replace("?", -99999, inplace=True)
 df.drop(["id"], 1, inplace=True)
 X = np.array(df.drop(["class"], 1))
